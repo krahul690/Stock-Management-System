7ff5054265aa2848127b3246816f2a02353958f1
Rahul Kumar Pasawan <114606478+rahuldhattarwalh@users.noreply.github.com>
2024-03-29 16:56:10 +0530

    add addOrder() method

--- src/emart/dao/OrderDAO.java
+++ src/emart/dao/OrderDAO.java
@@ -1,10 +1,15 @@
 package emart.dao;
 
+import com.mysql.cj.xdevapi.PreparableStatement;
 import emart.dbutil.DBConnection;
+import emart.pojo.ProductsPojo;
+import emart.pojo.UserProfile;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class OrderDAO {
 
@@ -21,7 +26,21 @@ public class OrderDAO {
         int orderno = Integer.parseInt(orderid.substring(2)); //O-101 already in 1 index
 //        orderno = orderno + 1;
         orderno++;
-        return "O" + orderno;
+        return "O-" + orderno;
 
+    }
+
+    public static boolean addOrder(ArrayList<ProductsPojo> al, String orderid) throws SQLException {
+        Connection conn = DBConnection.getConnection();
+        PreparedStatement ps = conn.prepareStatement("Insert into orders values(?,?,?,?)");
+        int count = 0;
+        for (ProductsPojo p : al) {
+            ps.setString(1, orderid);
+            ps.setString(1, p.getProductId());
+            ps.setInt(3, p.getQuantity());
+            ps.setString(4, UserProfile.getUserid());
+            count = count + ps.executeUpdate();
+        }
+         return count==al.size();
     }
 }
